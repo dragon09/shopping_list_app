4807d900f303bee48f6fc59d6802415bb1ec098c
Victoria Fajardo <victoria.fajardo@gmail.com>
2016-08-22 22:44:28 -0500

    checkbox strikethrough

--- js/app.js
+++ js/app.js
@@ -9,14 +9,26 @@ $(document).ready(function () {
 //   }
 // });
 
+$("#input").append('<span><input type="checkbox" class="check">' +
+$("#product").val() + '<br /></span');
 
-        $('.list').append('<input type="checkbox" name="myCheckbox" />');
-        // $(".list").append("<li class='item'>" + toAddNum + " " + toAdd + "</li>");
-        $(".list").append("<li class='item'>" + toAdd + " " + "</li>");
-        // $("#list").click(function() {
-        $(document).on("click", "item", function () {
-            $(this).wrap("<strike>");
-        })
+$("input:checkbox").click(function () {
+  var $this = $(this);
+  if (this.checked) {
+    $this.parent().addClass('completed');
+
+  } else {
+    $this.parent().removeClass('completed');
+  }
+});
+
+        // $('.list').append('<input type="checkbox" name="myCheckbox" />');
+        // // $(".list").append("<li class='item'>" + toAddNum + " " + toAdd + "</li>");
+        // $(".list").append("<li class='item'>" + toAdd + " " + "</li>");
+        // // $("#list").click(function() {
+        // $(document).on("click", "item", function () {
+        //     $(this).wrap("<strike>");
+        // })
 
         // })
 });
@@ -29,7 +41,4 @@ $(document).ready(function () {
         $(this).remove();
 
     });
-
-
-
 });
